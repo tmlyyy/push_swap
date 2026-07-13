43a526f2e32e2e8f57e18c10a804a2f77e1677b5
tmlyyy <thvics22@gmail.com>
2026-07-13 13:14:18 -0300

    Update algorithm_complex.c

--- algorithm_complex.c
+++ algorithm_complex.c
@@ -6,7 +6,7 @@
 /*   By: thamoliv <thamoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 17:45:12 by thamoliv          #+#    #+#             */
-/*   Updated: 2026/07/13 12:36:10 by thamoliv         ###   ########.fr       */
+/*   Updated: 2026/07/13 13:11:38 by thamoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 static int	partition_a(t_data *data, int size, int print)
 {
 	int	pushed;
-	int	pivot;
+	int	rotated;
 	int	i;
+	int	pivot;
 
 	pushed = 0;
+	rotated = 0;
+	i = size;
 	pivot = get_median(data->stack_a, size);
-	i = -1;
-	while (++i < size)
+	while (i-- > 0)
 	{
 		if (data->stack_a->index < pivot)
 		{
@@ -29,21 +31,29 @@ static int	partition_a(t_data *data, int size, int print)
 			pushed++;
 		}
 		else
+		{
 			op_ra(data, print);
+			rotated++;
+		}
 	}
+	if (size != data->size_a + pushed)
+		while (rotated--)
+			op_rra(data, print);
 	return (pushed);
 }
 
 static int	partition_b(t_data *data, int size, int print)
 {
 	int	pushed;
-	int	pivot;
+	int	rotated;
 	int	i;
+	int	pivot;
 
 	pushed = 0;
+	rotated = 0;
+	i = size;
 	pivot = get_median(data->stack_b, size);
-	i = -1;
-	while (++i < size)
+	while (i-- > 0)
 	{
 		if (data->stack_b->index >= pivot)
 		{
@@ -51,8 +61,14 @@ static int	partition_b(t_data *data, int size, int print)
 			pushed++;
 		}
 		else
+		{
 			op_rb(data, print);
+			rotated++;
+		}
 	}
+	if (size != data->size_b + pushed)
+		while (rotated--)
+			op_rrb(data, print);
 	return (pushed);
 }
 
@@ -64,11 +80,6 @@ void	quicksort_a(t_data *data, int size, int print)
 	{
 		if (size == 2 && data->stack_a->index > data->stack_a->next->index)
 			op_sa(data, print);
-		return ;
-	}
-	if (size == 3)
-	{
-		sort_three_a(data, print);
 		return ;
 	}
 	pushed = partition_a(data, size, print);
@@ -84,13 +95,6 @@ void	quicksort_b(t_data *data, int size, int print)
 	{
 		if (size == 2 && data->stack_b->index < data->stack_b->next->index)
 			op_sb(data, print);
-		while (size-- > 0)
-			op_pa(data, print);
-		return ;
-	}
-	if (size == 3)
-	{
-		sort_three_b(data, print);
 		while (size-- > 0)
 			op_pa(data, print);
 		return ;
